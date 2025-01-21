5a22d185252710bb6b4d66c6338a65d31ec9d3fa
busami-star <victorbusami1@gmail.com>
2025-01-22 00:36:36 +0300

    login commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,33 @@
-const Login = () => {
-  return <div>Login</div>;
-};
+"use client"
+import React from 'react'
+import { Axios } from 'axios'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
-export default Login;
+
+
+const login = () => {
+
+  const [user, setUser] = React.useState({
+    email:'',
+    password:'',
+    
+  })
+  const onlogin = async () => {}
+
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <h1 className='mb-4'>login</h1>
+      <hr />
+
+      <label htmlFor="email">email</label>
+      <input className='p-2' type="text" id='email' value={user.email} onChange={(e) => setUser({...user,email: e.target.value})} placeholder='email' />
+      <label htmlFor="password">password</label>
+      <input className='p-2' type="text" id='password' value={user.password} onChange={(e) => setUser({...user,password: e.target.value})} placeholder='password' />
+      <button onClick={onlogin} className='p-2 border border-gray-400 rounded-lg mb-4 mt-4 focus:outline-none focus:border-gray-900'>login here</button>
+      <Link href='/signup'>visit signup page  </Link>
+    </div>
+  )
+}
+
+export default login
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -16,8 +16,17 @@ const signup = () => {
   const onSignup = async () => {}
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen'>
-      signup
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <h1 className='mb-4'>signup</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input className='p-2' type="text" id='username' value={user.username} onChange={(e) => setUser({...user,username: e.target.value})} placeholder='username' />
+      <label htmlFor="email">email</label>
+      <input className='p-2' type="text" id='email' value={user.email} onChange={(e) => setUser({...user,email: e.target.value})} placeholder='email' />
+      <label htmlFor="password">password</label>
+      <input className='p-2' type="text" id='password' value={user.password} onChange={(e) => setUser({...user,password: e.target.value})} placeholder='password' />
+      <button onClick={onSignup} className='p-2 border border-gray-400 mt-4 rounded-lg mb-4 focus:outline-none focus:border-gray-900'>signup here</button>
+      <Link href='/login'>visit login page  </Link>
     </div>
   )
 }
